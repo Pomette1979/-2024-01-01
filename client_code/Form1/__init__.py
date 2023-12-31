b885e37480ea89c4d3d9214a12c4f04c28adb18e
new_21_@hotmail.com <new_21_@hotmail.com>
2023-12-31 21:16:50 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,9 +1,28 @@
 from ._anvil_designer import Form1Template
 from anvil import *
 
+
 class Form1(Form1Template):
+ 
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    # Any code you write here will run before the form opens.
+    # Any code you write here will run before the form open
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+   anvil.server.call_s("show1_message",self.text_box_.text)
+    pass
+
+  def button_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    anvil.server.call_s("show2_message",self.text_box_.text)
+    pass
+
+  def button_3_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    anvil.server.call_s("show2_message",self.text_box_.text)
+    pass
+
+
